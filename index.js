a9cdbb34bab105f6a6fe1ffecef0b39abe94c516
tolyaganzin <tolyaganzin@mail.ru>
2017-02-10 15:35:39 +0200

    add monk. New models in db

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 var express = require('express');
-var path    = require("path");
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/SpeedAngularApp');
+
+var routesV1 = require('./routes/api/v1/index');
+
 var app = express();
 // var config          = require('./libs/config');
-var bodyParser = require('body-parser');
-var Db = require('mongodb').Db;
-var Server = require('mongodb').Server;
-// var ObjectId = require('mongodb').ObjectId;
-var db = new Db('SpeedAngularApp', new Server('localhost', 27017));
+// var Server = require('mongodb').Server;
+// // var ObjectId = require('mongodb').ObjectId;
+// var db = new Db('SpeedAngularApp', new Server('localhost', 27017));
 
-
+app.use(logger('dev'));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(cookieParser());
 app.use(express.static('public'));
-
 // Make our db accessible to our router
 app.use(function(req,res,next){
     req.db = db;
@@ -22,7 +30,7 @@ app.use(function(req,res,next){
 
 ////////////////////// Routes
 // api
-app.use('/api/v1', require('./routes/api/v1/index'));
+app.use('/api/v1', routesV1);
 // main page
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
@@ -38,3 +46,5 @@ app.use(function(req, res, next) {
 app.listen(3300, function () {
   console.log('Example app listening on port 3300!');
 });
+
+module.exports = app;
--- routes/api/v1/user/index.js
+++ routes/api/v1/user/index.js
@@ -1,69 +1,44 @@
 var express = require('express');
 var router = express.Router();
-// var Db = require('mongodb').Db;
-// var Server = require('mongodb').Server;
 var ObjectId = require('mongodb').ObjectId;
 
 
-// var db = new Db('SpeedAngularApp', new Server('localhost', 27017));
-
-
 router.get('/get-users', function(req, res) {
-  req.db.open(function(err, db) {
-    var collection = db.collection("users");
-    // Get a all
-    collection.find().toArray(function(err, result) {
-      res.send(result);
-      db.close();
-    }); //collection
-  }); //req.db
+  var db = req.db;
+  var collection = db.get('users');
+  // Get a all
+  collection.find({},{},function(e,docs){
+      res.send(docs);
+  });
 }); //router
 
 router.put('/add-user', function (req, res){
-  req.db.open(function(err, db) {
-    var collection = db.collection("users");
-    //  Isert a one
-    collection.insert(req.body);
-    collection.find().toArray(function(err, result) {
-      res.send(result);
-      db.close();
-    }); //collection
-  }); //req.db
+  var db = req.db;
+  var collection = db.get("users");
+  //  Isert a one
+  collection.insert(req.body);
+  collection.find({},{},function(e,docs){
+      res.send(docs);
+  });
 }); //router
 
 router.put('/update-user/:_id', function (req, res){
-  req.db.open(function(err, db) {
-    var collection = db.collection("users");
-    //  Update a one
-    collection.update({"_id": new ObjectId(req.params._id)}, req.body, function(err, result) {
-        if (err) {
-          console.log(err);
-          res.send(result);
-          db.close();
-        } else {
-          res.send(result);
-          db.close();
-        }
-    }); //collection
-  }); //req.db
+  var db = req.db;
+  var collection = db.get("users");
+  //  Update a one
+  collection.update({_id: req.params._id}, req.body);
+  res.send('ok');
 }); //router
 
 router.delete('/del-user/:_id', function (req, res){
-  req.db.open(function(err, db) {
-    var collection = db.collection("users");
-    // Delete a one
-    collection.remove({_id: new ObjectId(req.params._id)}, function(err) {
-        if (err) {
-            console.log(err);
-            req.db.close();
-        } else {
-          collection.find().toArray(function(err, result) {
-            res.send(result);
-            req.db.close();
-          });
-        }
-    }); //collection
-  }); //req.db
+  var db = req.db;
+  var collection = db.get("users");
+  // Delete a one
+  collection.remove({_id: req.params._id});
+  collection.find({},{},function(e,docs){
+      res.send(docs);
+  });
+
 }); //router
 
 module.exports = router;
